5524b5f59dd06d4e05731ad16bdc8c1bb1718b70
IpoLa <akramchat@hotmail.com>
2026-02-24 10:42:26 +0100

    prod

--- src/main.ts
+++ src/main.ts
@@ -123,7 +123,7 @@ async function bootstrap() {
     });
 
     const port = process.env.PORT || 3001;
-    await app.listen(port);
+    await app.listen(port, '0.0.0.0');
     console.log(`🚀 Application démarrée sur: http://localhost:${port}`);
     console.log(`📚 Documentation Swagger: http://localhost:${port}/api/docs`);
     console.log(`🔓 CORS allowed origins:`, allowedOrigins);
